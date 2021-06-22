3b83dc1178e94075f37c269194063da893a35df7
Hellom Oliveira <hellom-mendes@hotmail.com>
2021-06-22 03:58:56 +0100

    Aula 2 - part 1

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,8 @@
+import  "reflect-metadata";
 import express from "express";
 
-// @types/express
+import "./database";
+
 const app = express();
 
-/* 
-    * GET       => Buscar uma informação
-    * POST      => Inserir (criar) uma informação
-    * PUT       => Alterar uma informação (atualização)
-    * DELETE    => Remover uma informação
-    * PATH      => Alterar uma informação especifica
-*/
-
-app.get("/test", (req, res) => {
-    //req => tudo que está entrando
-    //res => tudo que esta saindo
-    return res.send("ola nlw")
-});
-
-app.post("/test-post", (req, res) => {
-    return res.send("ola NLW metodo Post" )
-})
-
-// http://localhost:3000
 app.listen(3000, () => console.log("Server is running!"))
